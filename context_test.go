327b0c1bbd0d6022013f777ea9a857a04ddcb34a
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-10-24 22:25:16 +0700

    how to create context

---
+++ context_test.go
@@ -0,0 +1,15 @@
+package lesson_golang_context_pzn
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nuradyCloud/lesson-golang-context-pzn
+
+go 1.21
